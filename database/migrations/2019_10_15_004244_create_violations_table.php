0fe2fb0c20ba9ae025d3b2078dd65b83e40c7672
Full-Stack Developer <paul.marquita@fourello.com>
2019-10-15 08:44:54 +0800

    Violations Commit

---
+++ database/migrations/2019_10_15_004244_create_violations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViolationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('violations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('plate_number')->nullable();
+            $table->string('owner')->nullable();
+            $table->string('reported')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('violations');
+    }
+}
